56456f9e8759a9683adaa6a3aa64af6e7df28fd1
Avery <averywoodlief@gmail.com>
2026-09-12 19:02:52 -0500

    top line specifies heightxwidth of image

--- helpers/polyfile_reader.py
+++ helpers/polyfile_reader.py
@@ -4,10 +4,25 @@ def read_polyfile(filename : str) -> list:
     alias = {"P":"poly", "p":"poly", "poly":"poly", "polygon":"poly"}
     calls = []
     polyfile = find_file(folder_name="objects", file_name=filename).open("r").read().split("\n")
+    if len(polyfile) >= 1:
+        size = polyfile[0]
+        if not re.search(r"\d+x\d+", size):
+            raise ValueError("dimensions not specified, broken image file")
+        height, width = tuple(map(int, re.findall(r"\d+", size)))
+        calls.append({"size":(height, width)})
+        polyfile = polyfile[1:]
+    existing_layers = set()
     for line in polyfile:
+        if len(line) == 0:
+            continue
         cmd, layer, color, points = tuple(line.split("|"))
         r, g, b, a = tuple(map(int, re.findall(r"\d+", color)))
-        call = {alias.get(cmd):{"layer":int(layer), "color":(r, g, b, a)}}
+        numeric_layer = abs(int(layer))
+        if len(existing_layers)>0:
+            if numeric_layer > max(existing_layers) + 1:
+                numeric_layer = max(existing_layers) + 1
+        existing_layers.add(numeric_layer)
+        call = {alias.get(cmd):{"layer":numeric_layer, "color":(r, g, b, a)}}
         call[alias.get(cmd)]["points"] = []
         point_list_raw = points.split(";")
         for point in point_list_raw:
