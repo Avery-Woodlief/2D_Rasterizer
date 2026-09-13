97f47f07108199d01c64d125c17a823e7780c1ae
Avery <averywoodlief@gmail.com>
2026-09-12 19:03:09 -0500

    path issues with root

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -1,7 +1,11 @@
 from collections.abc import Callable
 from pathlib import Path
+import re
 
 ROOT = Path.cwd() # '...'/Image_Processing
+if re.search(r"2D_Rasterizer", str(ROOT)) is not None:
+    _, b = re.search(r"2D_Rasterizer", str(ROOT)).span()
+    ROOT = Path(str(ROOT)[:b])
 
 def validate_file(func : Callable[[str | None, str], Path]):
     def wrapper(*args, **kwargs):
@@ -13,8 +17,8 @@ def validate_file(func : Callable[[str | None, str], Path]):
 
 @validate_file
 def find_file(**kw) -> Path | str:
-    folder_name = kw.get("folder_name", None)
-    file_name = kw.get("file_name", None)
+    folder_name = kw.get("folder_name", kw.get("foldername", None))
+    file_name = kw.get("file_name", kw.get("filename", None))
     if isinstance(folder_name, (str, Path)) and isinstance(file_name, (str, Path)):
         expected_directory = ROOT / folder_name
         expected_location = expected_directory / file_name
@@ -22,8 +26,8 @@ def find_file(**kw) -> Path | str:
     return ROOT / file_name if isinstance(file_name, (str, Path)) else "N/A"
 
 def resolve_path(**kw) -> tuple[Path | str, Path | str]:
-    folder_name = kw.get("folder_name", None)
-    file_name = kw.get("file_name", None)
+    folder_name = kw.get("folder_name", kw.get("foldername", None))
+    file_name = kw.get("file_name", kw.get("filename", None))
     if isinstance(folder_name, (str, Path)) and isinstance(file_name, (str, Path)):
         absolute_path = ROOT / folder_name / file_name
         relative_path = Path(folder_name) / file_name
@@ -42,4 +46,6 @@ def overwrite_file(location: Path, what) -> None:
 
 if __name__ == "__main__":
     #print(find_file(folder_name = 8, file_name = 1))
-    print(resolve_path())
+    #print(resolve_path())
+    print(ROOT)
+    print(find_file(filename="__init__.py"))
