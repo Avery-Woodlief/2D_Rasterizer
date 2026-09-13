9de34faaac35a26719f019a30c38576458c3e935
Avery <averywoodlief@gmail.com>
2026-09-12 19:03:20 -0500

    main

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from buffers.drawing import Drawing
 from helpers.polyfile_reader import read_polyfile
 
-drawing = Drawing(60, 60)
-calls = read_polyfile("image.poly")
+calls = read_polyfile("generated_palete.poly")
+HEIGHT, WIDTH = calls[0]["size"]
+drawing = Drawing(HEIGHT, WIDTH)
+calls = calls[1:]
 for call in calls:
     getattr(drawing, "poly")(**call.get("poly"))
 
-drawing.imsave("test", drawing.framebuffer)
+from skimage.morphology import disk
+
+image = drawing.clean_image(disk(0))
+drawing.imsave("generated_palete", image)
